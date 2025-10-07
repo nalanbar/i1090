65d7e29bb07aef3bb97901de47694f53d817e6ba
nalanbar <nabil@alanbar.us>
2025-10-07 14:48:41 -0400

    Initial Commit

---
+++ i1090/i1090App.swift
@@ -0,0 +1,17 @@
+//
+//  i1090App.swift
+//  i1090
+//
+//  Created by Nabil on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct i1090App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
